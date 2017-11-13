bb9d1d31082ac62eef24fcf4c45faec463435ad0
wangyi0559 <969392953@qq.com>
2017-11-13 19:53:13 +0800

    invoke 执行

--- fabricExample124/fabric_invoke.sh
+++ fabricExample124/fabric_invoke.sh
@@ -5,11 +5,11 @@ a=$(( $NUM1MINUTE / 10 ))
 b=$(( $NUM1MINUTE % 10 ))
 for((i=1;i<=$a;i++));
 do
-nohup curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=10 >/dev/null 2>&1 &
+curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=10
 sleep 1s
 done
 if [ $b != "0" ]
 then
-nohup curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=$b >/dev/null 2>&1 &
+curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=$b
 fi
 echo "success"
