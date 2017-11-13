b7867063194ddb294057a39b95d03eed5ddf6e5b
wangyi0559 <969392953@qq.com>
2017-11-13 19:20:26 +0800

    交易修改

--- fabricExample124/fabric_invoke.sh
+++ fabricExample124/fabric_invoke.sh
@@ -1,4 +1,15 @@
 #!/bin/sh	
 # author:wang yi
 NUM1MINUTE=$*
-curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=$NUM1MINUTE
+a=$(( $NUM1MINUTE / 10 ))
+b=$(( $NUM1MINUTE % 10 ))
+for((i=1;i<=$a;i++));
+do
+nohup curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=10 >/dev/null 2>&1 &
+sleep 1s
+done
+if [ $b != "0" ]
+then
+nohup curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=$b >/dev/null 2>&1 &
+fi
+echo "success"
