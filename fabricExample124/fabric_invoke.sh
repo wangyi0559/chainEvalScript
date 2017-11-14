a0d64fa30b6c1c80730b34827faf77b58df5c4ff
wangyi0559 <969392953@qq.com>
2017-11-14 16:23:34 +0800

    invoke 修改

--- fabricExample124/fabric_invoke.sh
+++ fabricExample124/fabric_invoke.sh
@@ -1,15 +1,4 @@
 #!/bin/sh	
 # author:wang yi
 NUM1MINUTE=$*
-a=$(( $NUM1MINUTE / 10 ))
-b=$(( $NUM1MINUTE % 10 ))
-for((i=1;i<=$a;i++));
-do
-curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=10
-sleep 5s
-done
-if [ $b != "0" ]
-then
-curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=$b
-fi
-echo "success"
+curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=$NUM1MINUTE
