3034b4edbfb97c74648a9e9c10812fdeaf222736
wangyi0559 <969392953@qq.com>
2017-11-15 15:11:57 +0800

    超时设置

--- fabricExample124/fabric_invoke.sh
+++ fabricExample124/fabric_invoke.sh
@@ -1,4 +1,4 @@
 #!/bin/sh	
 # author:wang yi
 NUM1MINUTE=$*
-curl -s -X GET 127.0.0.1:8080/api/invokeCC?num=$NUM1MINUTE
+curl -s -X GET --connect-timeout 3000 -m 3000 127.0.0.1:8080/api/invokeCC?num=$NUM1MINUTE
