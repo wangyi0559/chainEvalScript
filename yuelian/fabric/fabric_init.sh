772339d268a1fa43dc503273654abfba25cd8326
wangyi0559 <969392953@qq.com>
2017-11-20 18:55:51 +0800

    init修改

--- yuelian/fabric/fabric_init.sh
+++ yuelian/fabric/fabric_init.sh
@@ -1,38 +1,32 @@
 #!/bin/sh	
 # author:wang yi
-function installChaincode(){
-    docker run --rm --privileged=true \
-    --network=host \
-    -d registry.cn-hangzhou.aliyuncs.com/wangyi0559/eval-init curl 127.0.0.1:8080/api/installChaincode 
-}
-function instantiateChaincode(){
-    docker run --rm --privileged=true \
-    --network=host \
-    -d registry.cn-hangzhou.aliyuncs.com/wangyi0559/eval-init curl 127.0.0.1:8080/api/instantiateChaincode 
-}
 PEER_INDEX=$(cat /chain/PEER_INDEX)
 function initFabric(){
     case $PEER_INDEX in
         3)  {
-            /bin/bash /chain/chainEvalScript/yuelian/fabric/init/init-p0o1.sh
+            curl 127.0.0.1:8080/api/users
+            sleep 5s
+            curl 127.0.0.1:8080/api/createChannel 
+            sleep 5s 
+            curl 127.0.0.1:8080/api/joinchannel 
             sleep 10s
         }
         ;;
         4|5)  {
-            sleep 20s
+            sleep 30s
             sleep $PEER_INDEX
-            /bin/bash /chain/chainEvalScript/yuelian/fabric/init/init-p1o1.sh
+            curl 127.0.0.1:8080/api/users
             sleep 10s
         }
         ;;
         6)  {
-            sleep 30s
+            sleep 40s
             sleep $PEER_INDEX
-            /bin/bash /chain/chainEvalScript/yuelian/fabric/init/init-p1o1.sh
-            sleep 20s
-            installChaincode
+            curl 127.0.0.1:8080/api/users
+            sleep 5s
+            curl 127.0.0.1:8080/api/installChaincode 
             sleep 10s
-            instantiateChaincode
+            curl 127.0.0.1:8080/api/instantiateChaincode 
             sleep 10s
         }
         ;;
