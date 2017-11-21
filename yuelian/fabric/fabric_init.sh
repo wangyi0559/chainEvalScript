5e0e94a5cbdf9635929599fc0a8136d0ba5d5c07
wangyi0559 <969392953@qq.com>
2017-11-21 18:43:05 +0800

    init 修改

--- yuelian/fabric/fabric_init.sh
+++ yuelian/fabric/fabric_init.sh
@@ -27,7 +27,7 @@ function initFabric(){
         6)  {
             sleep 40s
             curl 127.0.0.1:8080/api/users
-            sleep 30s
+            sleep 50s
             curl 127.0.0.1:8080/api/installChaincode 
             sleep 10s
             curl 127.0.0.1:8080/api/instantiateChaincode 
